a3d945c1cd1fee4467c0d2206de59531fb57a20f
Dan <danieladamspencer@users.noreply.github.com>
2020-09-06 09:42:58 -0400

    Getting further on the BTRR vignette
    Also started a skeleton for the BTRT vignette

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Results from the simulated sample Bayesian Tensor Response Regression
+#'
+#' This object is a list containing the results of the
+#' \code{BTRR_single_subject} function for ranks 1, 2, and 3 based on the
+#' simulated data created in the vignette.
+#'
+#' @format A list of three \code{BTRR_result} objects.
+"BTRR_results"
